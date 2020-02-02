5ec56543b92836f5715332482211a18d8eb9def5
Yusuke Tanaka <wing0920@gmail.com>
2020-02-02 19:34:12 +0900

    Add `input!` macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,5 @@
-use std::io::stdin;
+mod input;
+mod read;
 
-// cf: https://github.com/vain0x/scan-bench
-#[macro_export]
-macro_rules! read {
-    ([$t:ty] ; $n:expr) =>
-        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
-    ($($t:ty),+ ; $n:expr) =>
-        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
-    ([$t:ty]) =>
-        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
-    ($t:ty) =>
-        (rl().parse::<$t>().unwrap());
-    ($($t:ty),*) => {{
-        let buf = rl();
-        let mut w = buf.split_whitespace();
-        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
-    }};
-}
-
-pub fn rl() -> String {
-    let mut buf = String::new();
-    stdin().read_line(&mut buf).unwrap();
-    buf.trim_end().to_owned()
-}
+pub use input::*;
+pub use read::*;
