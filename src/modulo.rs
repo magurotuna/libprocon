2a992cbb37b096e058800c2f535e3da17bfe300d
Yusuke Tanaka <wing0920@gmail.com>
2020-02-11 17:52:36 +0900

    Impelemnt `Comb` to calculate combination mod

--- src/modulo.rs
+++ src/modulo.rs
@@ -50,41 +50,117 @@ pub fn mod_inv(a: u64, m: u64) -> u64 {
     u as u64
 }
 
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(3, 7), 1);
-    assert_eq!(gcd(10, 5), 5);
+/// 二項係数を mod のもとで求める
+/// cf. [よくやる二項係数 (nCk mod. p)、逆元 (a^-1 mod. p) の求め方 - けんちょんの競プロ精進記録](http://drken1215.hatenablog.com/entry/2018/06/08/210000)
+pub struct Comb {
+    max_size: usize,
+    modulo: usize,
+    factorical_table: Vec<usize>,
+    factorical_inverse_table: Vec<usize>,
+    inverse_table: Vec<usize>,
 }
 
-#[test]
-fn test_lcm() {
-    assert_eq!(lcm(4, 6), 12);
-    assert_eq!(lcm(14, 21), 42);
-}
+impl Comb {
+    pub fn new(max_size: usize, modulo: usize) -> Self {
+        let max_size = std::cmp::max(10, max_size);
 
-#[test]
-fn test_mod_pow() {
-    let m = 1_000_000_007;
-    let x = 1234;
-    let mut t = 1;
-    for i in 0..1000 {
-        assert_eq!(mod_pow(x, i, m), t);
-        t = t * x % m;
+        // 10^7 までしか実用的な速度で計算できない
+        assert!(max_size <= 10_000_000);
+
+        let mut factorical_table = vec![0; max_size];
+        let mut factorical_inverse_table = vec![0; max_size];
+        let mut inverse_table = vec![0; max_size];
+        factorical_table[0] = 1;
+        factorical_table[1] = 1;
+        factorical_inverse_table[0] = 1;
+        factorical_inverse_table[1] = 1;
+        inverse_table[1] = 1;
+        for i in 2..max_size {
+            factorical_table[i] = factorical_table[i - 1] * i % modulo;
+            inverse_table[i] = modulo - inverse_table[modulo % i] * (modulo / i) % modulo;
+            factorical_inverse_table[i] =
+                factorical_inverse_table[i - 1] * inverse_table[i] % modulo;
+        }
+        Self {
+            max_size,
+            modulo,
+            factorical_table,
+            factorical_inverse_table,
+            inverse_table,
+        }
+    }
+
+    pub fn calc(&self, n: usize, k: usize) -> usize {
+        if n < k {
+            0
+        } else {
+            self.factorical_table[n]
+                * (self.factorical_inverse_table[k] * self.factorical_inverse_table[n - k]
+                    % self.modulo)
+                % self.modulo
+        }
     }
 }
 
-#[test]
-fn test_mod_inv() {
-    let tests = [
-        (1, 13, 1),
-        (2, 13, 7),
-        (3, 13, 9),
-        (4, 13, 10),
-        (5, 13, 8),
-        (6, 13, 11),
-    ];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    for test in tests.iter() {
-        assert_eq!(test.2, mod_inv(test.0, test.1));
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(3, 7), 1);
+        assert_eq!(gcd(10, 5), 5);
+    }
+
+    #[test]
+    fn test_lcm() {
+        assert_eq!(lcm(4, 6), 12);
+        assert_eq!(lcm(14, 21), 42);
+    }
+
+    #[test]
+    fn test_mod_pow() {
+        let m = 1_000_000_007;
+        let x = 1234;
+        let mut t = 1;
+        for i in 0..1000 {
+            assert_eq!(mod_pow(x, i, m), t);
+            t = t * x % m;
+        }
+    }
+
+    #[test]
+    fn test_mod_inv() {
+        let tests = [
+            (1, 13, 1),
+            (2, 13, 7),
+            (3, 13, 9),
+            (4, 13, 10),
+            (5, 13, 8),
+            (6, 13, 11),
+        ];
+
+        for test in tests.iter() {
+            assert_eq!(test.2, mod_inv(test.0, test.1));
+        }
+    }
+
+    #[test]
+    fn test_comb() {
+        let modulo = 1_000_000_007;
+        let comb = Comb::new(1_000_000, modulo);
+        let tests = [
+            (2, 1, 2),
+            (20, 15, 15504),
+            (20, 5, 15504),
+            (25, 15, 3268760),
+            (50, 10, 272278100),
+            (666666, 333333, 151840682),
+            (10, 9999, 0),
+        ];
+
+        for test in tests.iter() {
+            assert_eq!(test.2, comb.calc(test.0, test.1));
+        }
     }
 }
