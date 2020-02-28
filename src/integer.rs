6724ebd79934c3e94239817b9e4b9c8c071e57c6
Yusuke Tanaka <wing0920@gmail.com>
2020-02-29 00:45:50 +0900

    Generalize integer functions

--- src/integer.rs
+++ src/integer.rs
@@ -1,44 +1,126 @@
 use cargo_snippet::snippet;
 
-#[snippet("GCD")]
-pub fn gcd(a: u64, b: u64) -> u64 {
-    if b == 0 {
+#[snippet("INT_TRAIT")]
+pub trait Int:
+    std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Mul<Output = Self>
+    + std::ops::Div<Output = Self>
+    + std::ops::Rem<Output = Self>
+    + std::hash::Hash
+    + PartialEq
+    + Eq
+    + PartialOrd
+    + Ord
+    + Copy
+{
+    fn zero() -> Self;
+    fn one() -> Self;
+    fn next(self) -> Self;
+    fn prev(self) -> Self;
+    fn sqrt_floor(self) -> Self;
+}
+
+macro_rules! impl_int_for_numerics {
+    ( $( $t: ty )* ) => (
+        $(
+            impl Int for $t {
+                fn zero() -> Self {
+                    0
+                }
+                fn one() -> Self {
+                    1
+                }
+                fn next(self) -> Self {
+                    self + Self::one()
+                }
+                fn prev(self) -> Self {
+                    self - Self::one()
+                }
+                fn sqrt_floor(self) -> Self {
+                    if self < Self::zero() {
+                        return Self::zero();
+                    }
+                    let two = Self::one().next();
+                    let mut ok = Self::zero();
+                    let mut ng = self.next();
+                    while ng - ok > 1 {
+                        let mid = (ng + ok) / two;
+                        if mid * mid <= self {
+                            ok = mid;
+                        } else {
+                            ng = mid;
+                        }
+                    }
+                    ok
+                }
+            }
+        )*
+    )
+}
+
+#[snippet("INT_TRAIT")]
+impl_int_for_numerics!(u8 i8 u16 i16 u32 i32 u64 i64 usize isize);
+
+#[snippet("GCD", include = "INT_TRAIT")]
+pub fn gcd<T>(a: T, b: T) -> T
+where
+    T: Int,
+{
+    if b == T::zero() {
         a
     } else {
         gcd(b, a % b)
     }
 }
 
-#[snippet("LCM", include = "GCD")]
-pub fn lcm(a: u64, b: u64) -> u64 {
+#[snippet("LCM", include = "GCD", include = "INT_TRAIT")]
+pub fn lcm<T>(a: T, b: T) -> T
+where
+    T: Int,
+{
     a / gcd(a, b) * b
 }
 
-#[snippet("PRIME")]
-pub trait Prime {
-    fn lower_primes(&self) -> Vec<usize>;
-    fn factorize(&self) -> std::collections::HashMap<usize, usize>;
+#[snippet("PRIME", include = "INT_TRAIT")]
+pub trait Prime<T: Int> {
+    fn lower_primes(&self) -> Vec<T>;
+    fn factorize(&self) -> std::collections::HashMap<T, usize>;
 }
 
-#[snippet("PRIME")]
-impl Prime for usize {
+#[snippet("PRIME", include = "INT_TRAIT")]
+impl<T> Prime<T> for T
+where
+    T: Int,
+{
     /// エラトステネスの篩を用いてself以下の素数を求める
     /// 計算量: O(n log log n)
-    fn lower_primes(&self) -> Vec<usize> {
+    fn lower_primes(&self) -> Vec<T> {
         let &this = self;
         let mut v = Vec::new();
-        if this < 2 {
+        if this <= T::one() {
             return v;
         }
-        let mut deque = (2..(this + 1)).collect::<std::collections::VecDeque<usize>>();
+        let mut deque = std::collections::VecDeque::new();
+        let mut t = T::one().next(); // 2, which is the first prime number
+
+        // prepare `2, 3, 4, ..., this` sequence
+        while t <= this {
+            deque.push_back(t);
+            t = t.next();
+        }
 
         let mut p = match deque.pop_front() {
             Some(x) => x,
             None => return v,
         };
         v.push(p);
-        while p as f64 <= (this as f64).sqrt() {
-            deque = deque.iter().filter(|&&x| x % p != 0).map(|x| *x).collect();
+        while p * p <= this {
+            deque = deque
+                .iter()
+                .filter(|&&x| x % p != T::zero())
+                .map(|x| *x)
+                .collect();
             p = match deque.pop_front() {
                 Some(x) => x,
                 None => return v,
@@ -52,18 +134,18 @@ impl Prime for usize {
     }
 
     /// エラトステネスの篩を用いてselfを素因数分解する
-    fn factorize(&self) -> std::collections::HashMap<usize, usize> {
+    fn factorize(&self) -> std::collections::HashMap<T, usize> {
         let mut ret = std::collections::HashMap::new();
-        let primes = ((*self as f64).sqrt() as usize).lower_primes();
+        let primes = self.sqrt_floor().lower_primes();
 
         let mut tmp = *self;
         for prime in primes {
-            while tmp % prime == 0 {
-                tmp /= prime;
+            while tmp % prime == T::zero() {
+                tmp = tmp / prime;
                 *ret.entry(prime).or_insert(0) += 1;
             }
         }
-        if tmp > 1 {
+        if tmp > T::one() {
             *ret.entry(tmp).or_insert(0) += 1;
         }
         ret
@@ -74,6 +156,25 @@ impl Prime for usize {
 mod tests {
     use super::*;
     use std::collections::HashMap;
+
+    #[test]
+    fn test_sqrt_floor() {
+        let tests = [
+            (0, 0),
+            (1, 1),
+            (2, 1),
+            (3, 1),
+            (4, 2),
+            (8, 2),
+            (9, 3),
+            (99, 9),
+            (100, 10),
+            (-1, 0),
+        ];
+        for test in tests.iter() {
+            assert_eq!(test.0.sqrt_floor(), test.1);
+        }
+    }
 
     #[test]
     fn test_gcd() {
