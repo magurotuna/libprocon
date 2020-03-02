2789edd7b35fb0549849070565016c70377f88e4
Yusuke Tanaka <wing0920@gmail.com>
2020-03-03 00:44:30 +0900

    Add size function to DisjointSet

--- src/union_find.rs
+++ src/union_find.rs
@@ -4,7 +4,8 @@ use cargo_snippet::snippet;
 #[derive(Debug, Clone)]
 pub struct DisjointSet {
     parent: Vec<usize>,
-    rank: Vec<usize>,
+    sizes: Vec<i64>,
+    rank: Vec<i64>,
 }
 
 #[snippet("DISJOINT_SET")]
@@ -12,12 +13,13 @@ impl DisjointSet {
     pub fn new(n: usize) -> Self {
         Self {
             parent: (0..n).collect(),
+            sizes: vec![1; n],
             rank: vec![0; n],
         }
     }
 
     pub fn root(&mut self, x: usize) -> Option<usize> {
-        if x > self.parent.len() {
+        if x >= self.parent.len() {
             None
         } else if self.parent[x] == x {
             Some(x)
@@ -43,8 +45,10 @@ impl DisjointSet {
         }
         if self.rank[x] < self.rank[y] {
             self.parent[x_root] = y_root;
+            self.sizes[y_root] += self.sizes[x_root];
         } else {
             self.parent[y_root] = x_root;
+            self.sizes[x_root] += self.sizes[y_root];
             if self.rank[x_root] == self.rank[y_root] {
                 self.rank[x_root] += 1;
             }
@@ -63,6 +67,10 @@ impl DisjointSet {
 
         x_root == y_root
     }
+
+    pub fn size(&mut self, x: usize) -> Option<i64> {
+        self.root(x).map(|r| self.sizes[r])
+    }
 }
 
 #[cfg(test)]
@@ -74,14 +82,35 @@ mod tests {
         let mut s = DisjointSet::new(10);
 
         s.unite(0, 1);
+        for i in 0..=1 {
+            assert_eq!(s.size(i), Some(2));
+        }
+        for i in 2..10 {
+            assert_eq!(s.size(i), Some(1));
+        }
+        assert_eq!(s.size(10), None);
         assert!(s.same(0, 1));
         assert!(!s.same(0, 2));
 
         s.unite(2, 3);
+        for i in 0..=3 {
+            assert_eq!(s.size(i), Some(2));
+        }
+        for i in 4..10 {
+            assert_eq!(s.size(i), Some(1));
+        }
+        assert_eq!(s.size(10), None);
         assert!(s.same(2, 3));
         assert!(!s.same(0, 2));
 
         s.unite(1, 3);
+        for i in 0..=3 {
+            assert_eq!(s.size(i), Some(4));
+        }
+        for i in 4..10 {
+            assert_eq!(s.size(i), Some(1));
+        }
+        assert_eq!(s.size(10), None);
         assert!(s.same(0, 2));
         assert!(s.same(1, 2));
         assert!(s.same(0, 3));
